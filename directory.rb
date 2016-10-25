030cde92436b30c43d12af5f304cca4d0a526cb1
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-10-25 18:48:53 +0100

    Refactored code

--- directory.rb
+++ directory.rb
@@ -13,29 +13,59 @@
 #   {name: "Norman Bates", cohort: :november}
 # ]
 
+@students = []
+
 def interactive_menu
-  students = []
   loop do
-    # show user list of options and ask for user input
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # read user input and save to a variable
-    selection = gets.chomp
-    # execute action
-    case selection
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  # show user list of options and ask for user input
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you meant, try again"
-    end
   end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #get first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add student hash to the array
+    @students << {name: name, cohort: :november}
+    if @students.count == 1
+      student_char = "student"
+    else
+      student_char = "students"
+    end
+    puts "Now we have #{@students.count} #{student_char}"
+    #get another name from the user
+    name = gets.chomp
+  end
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
 end
 
 def print_header
@@ -43,43 +73,21 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  if students.count == 1
+def print_footer
+  if @students.count == 1
     student_char = "student"
   else
     student_char = "students"
   end
-  puts "Overall, we have #{students.count} great #{student_char}"
+  puts "Overall, we have #{@students.count} great #{student_char}"
 end
 
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
-  #get first name
-  name = gets.chomp
-  #while the name is not empty, repeat this code
-  while !name.empty? do
-    #add student hash to the array
-    students << {name: name, cohort: :november}
-    if students.count == 1
-      student_char = "student"
-    else
-      student_char = "students"
-    end
-    puts "Now we have #{students.count} #{student_char}"
-    #get another name from the user
-    name = gets.chomp
-  end
-  #return the array of students
-  students
-end
+
 
 interactive_menu
