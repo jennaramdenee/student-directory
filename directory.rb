383cb9b9162c7049e3ea233953ee2f46c7cc71d6
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-10-25 19:21:54 +0100

    Added new save option

--- directory.rb
+++ directory.rb
@@ -26,6 +26,7 @@ def print_menu
   # show user list of options and ask for user input
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the students"
   puts "9. Exit"
 end
 
@@ -35,6 +36,8 @@ def process(selection)
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -88,6 +91,14 @@ def print_footer
   puts "Overall, we have #{@students.count} great #{student_char}"
 end
 
-
+def save_students
+  file = File.open('students.csv', 'w')
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 interactive_menu
